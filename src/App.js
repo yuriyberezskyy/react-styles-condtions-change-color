a1fa28d1af49c79dc23fb7d284703d646724caeb
yuriyberezskyy <bereza95@gmail.com>
2020-05-08 00:17:20 -0400

    added hover to css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
+import Radium from "radium";
 
 class App extends Component {
   state = {
@@ -52,6 +53,10 @@ class App extends Component {
       border: "1px solid blue",
       padding: "8px",
       cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black",
+      },
     };
 
     let persons = null;
@@ -73,6 +78,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black",
+      };
     }
 
     const classes = [];
@@ -98,4 +107,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
